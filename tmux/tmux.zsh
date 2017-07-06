3ab68d81042d00a24bf4f028a34b0845a51bb941
Dafydd Woods <giganticus@gmail.com>
2017-07-06 16:01:14 +0100

    Disabled TMUX because of AV incompat. Changed colours

--- tmux/tmux.zsh
+++ tmux/tmux.zsh
@@ -1,6 +1,6 @@
 # Fixes tmux colours
 alias tmux="TERM=xterm-256color tmux"
 
-if which tmux 2>&1 >/dev/null; then
-    test -z "$TMUX" && (tmux attach || tmux new-session)
-fi
+#if which tmux 2>&1 >/dev/null; then
+#    test -z "$TMUX" && (tmux attach || tmux new-session)
+#fi
